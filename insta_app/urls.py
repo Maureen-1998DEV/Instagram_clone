ff44af19ed89165a48d0dd38bfdd390cd442655e
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-10-19 00:40:49 +0300

    adding media

--- insta_app/urls.py
+++ insta_app/urls.py
@@ -5,16 +5,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$', views.home, name='sign'),
-    url(r'^home/$',views.index,name='index'),
-    url(r'^new/image$', views.new_image, name='new-image'),
-    url(r'^profile/$',views.profile,name='profile'),
-    url(r'^search/', views.search_results, name='search_results'),
-    url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
-    url(r'^profiles/(\d+)',views.profiles,name='profiles'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
+    url('^$', views.home, name='sign'),
+    url('^home/$',views.index,name='index'),
+    url('^new/image$', views.new_image, name='new-image'),
+    url('^profile/$',views.profile,name='profile'),
+    url('^search/', views.search_results, name='search_results'),
+    url('^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+    url('^profiles/(\d+)',views.profiles,name='profiles'),
+    url('^signup/$', views.signup, name='signup'),
+    url('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
