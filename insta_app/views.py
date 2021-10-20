b29da8fdaedd54b31f94945b0d286ac34e4269bf
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-10-20 14:04:02 +0300

    deploying

--- insta_app/views.py
+++ insta_app/views.py
@@ -14,8 +14,8 @@ from django.contrib.auth import login, authenticate
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
-from .token import account_activation_token
+
+
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 
